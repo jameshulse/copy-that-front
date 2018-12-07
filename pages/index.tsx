d46baa2396213f14dc87635306a946a480d77d63
James Mulholland <jmulholland1618@gmail.com>
2018-12-07 14:00:12 +0000

    wip

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Style = styled.div`
+  color: ${props => props.theme.purple};
+`;
+
+const content = `lorem`;
+
+const Home = () => <Style>{content}</Style>;
+
+export default () => <Home />;
