1e0ed1107bf7ef7728ee14ddfa599a1b51559193
James Mulholland <jmulholland1618@gmail.com>
2018-12-12 08:51:01 +0000

    refactor: .js => .ts(x)

---
+++ lib/withData.ts
@@ -0,0 +1,27 @@
+import withApollo from "next-with-apollo";
+import ApolloClient from "apollo-boost";
+// import yoga server endpoint
+import { endpoint } from "../config";
+import { IncomingHttpHeaders } from "http";
+
+interface CreateClientProps {
+  headers?: IncomingHttpHeaders;
+}
+
+// headers are needed for authentication
+function createClient({ headers }: CreateClientProps) {
+  return new ApolloClient({
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
+    request: async operation => {
+      await operation.setContext({
+        fetchOptions: {
+          // automatically include auth cookies
+          credentials: "include"
+        },
+        headers
+      });
+    }
+  });
+}
+
+export default withApollo(createClient);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import App, { Container } from 'next/app';
-import { ApolloProvider } from 'react-apollo';
-import withData from '../lib/withData';
-import Page from '../components/Page';
+import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
+import Page from "../components/Page";
 
 class CopyThatApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    let pageProps = { query: undefined };
     if (Component.getInitalProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
@@ -14,7 +14,7 @@ class CopyThatApp extends App {
   }
 
   render() {
-    const { Component, apollo, pageProps } = this.props;
+    const { Component, apollo, pageProps } = this.props as any;
     return (
       <Container>
         <ApolloProvider client={apollo}>
