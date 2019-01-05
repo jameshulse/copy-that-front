fde108d200ae062381228ceacd98de254756f397
James Mulholland <jmulholland1618@gmail.com>
2019-01-05 17:16:51 +0000

    add heroku settings

--- config.ts
+++ config.ts
@@ -1,2 +1,3 @@
 // client side (and therefore public!) config
 export const endpoint = `http://localhost:4001`;
+export const prodEnpoint = 'https://copy-that-prod.herokuapp.com'
--- lib/withData.ts
+++ lib/withData.ts
@@ -1,7 +1,7 @@
 import withApollo from "next-with-apollo";
 import ApolloClient from "apollo-boost";
 // import yoga server endpoint
-import { endpoint } from "../config";
+import { endpoint, prodEndpoint } from "../config";
 import { IncomingHttpHeaders } from "http";
 
 interface CreateClientProps {
@@ -11,7 +11,7 @@ interface CreateClientProps {
 // headers are needed for authentication
 function createClient({ headers }: CreateClientProps) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
     request: async operation => {
       await operation.setContext({
         fetchOptions: {
